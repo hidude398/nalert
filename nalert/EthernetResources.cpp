a175361abe83b21eb65ba2219503b77cd23fb23a
hidude398 <hidude398@gmail.com>
2022-12-06 12:26:22 -0500

    Added some ethernet definitions

---
+++ nalert/EthernetResources.cpp
@@ -0,0 +1,47 @@
+#include <pcap/pcap.h>
+#include <array>
+
+#include "EthernetResources.h"
+
+namespace layer_two {
+
+	// Here we assign the data pointer of the EthernetFrame class to be the
+	//     length of the captured data, and then copy the values of the data
+	//     data received from the capture into the EthernetFrame
+	EthernetFrame::EthernetFrame(pcap_pkthdr* cap_head, uint8_t* data_in) {
+		// If the capture has no fragments of FCS, copy normally. Otherwise, if the last 4 chars are the FCS, drop them
+		if (cap_head->len - cap_head->caplen >= 4) {
+			data = new uint8_t[cap_head->caplen];
+			std::memcpy(data, data_in, cap_head->caplen);
+		} else {
+			// When the actual length - captured length <4, make sure to drop any remaining bytes of the FCS.
+			data = new uint8_t[cap_head->caplen - (4 - (cap_head->len - cap_head->caplen) )];
+			std::memcpy(data, data_in, cap_head->caplen);
+		}
+	}
+
+	// Here we free the held memory when the class is destroyed
+	EthernetFrame::~EthernetFrame() {
+		delete data;
+	}
+
+	// Here, we return the first 6 characters of the data, which is a destination
+	//    Mac address
+	std::array<uint8_t, 6> EthernetFrame::get_dest_addr() {
+		std::array<uint8_t, 6> returnValue;
+		// Returns the first set of 6 chars
+		std::memcpy(returnValue.data(), data, 6);
+		return returnValue;
+	}
+
+	// Here, we return the next 6 characters of the data, which is a source
+	//     Mac address
+	std::array<uint8_t, 6> EthernetFrame::get_srce_addr() {
+		std::array<uint8_t, 6> returnValue;
+		// Returns the 2nd set of 6 chars
+		std::memcpy(returnValue.data(), data + 6, 6);
+		return returnValue;
+	}
+
+
+}
--- nalert/EthernetResources.h
+++ nalert/EthernetResources.h
@@ -1,8 +1,40 @@
 #pragma once
-
 #include <pcap.h>
+#include <array>
+#include <stdint.h>
+#include <boost/dynamic_bitset.hpp>
+#include "NetworkLayerResources.h"
 
-template <size_t T>
-class EthernetFrame {
+namespace layer_two {
 
-};
+
+	class EthernetFrame {
+	public:
+		// Precondition: Takes a pcap_pkthdr struct and an array of data from a npcap capture
+		// Postcondition: Produces an EthernetFrame which has different methods for accessing
+		//     different parts of the ethernet frame for decapsulation
+		EthernetFrame(pcap_pkthdr* cap_head, uint8_t* data);
+
+		// Precondition: Is called from an instance of an EthernetFrame
+		// Postcondition: The associated memory of that class is freed
+		~EthernetFrame();
+
+		// Precondition: an EthernetFrame exists
+		// Postcondition: Returns the destination address
+		std::array<uint8_t, 6> get_dest_addr();
+
+		// Precondition: an EthernetFrame exists
+		// Postcondition: Returns the source address as a 6 value std::Array
+		std::array<uint8_t, 6> get_srce_addr();
+
+	private:
+		uint8_t* data;
+		int len;
+	};
+
+	template <std::size_t T>
+	struct ethernetPayload {
+		std::array<uint8_t, T> payload;
+	};
+
+} // Close namespace layer_two
